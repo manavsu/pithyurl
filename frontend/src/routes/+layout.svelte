b4019444dda67a7d8af1c6d9e36c9e4f73120f6f
manav <manavjotsingh17@gmail.com>
2025-12-24 15:33:17 -0800

    updated front end

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,24 +1,11 @@
 <script lang="ts">
-	import '$lib/app.css';
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
 
-
-<style lang="postcss">
-	@import url('https://fonts.googleapis.com/css2?family=Patrick+Hand&display=swap');
-
-	:global(html) {
-	  @apply bg-black text-white;
-	  caret-color: white;
-	}
-	
-	* {
-		/* @apply scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-transparent; */
-		font-family: 'Patrick Hand', cursive;
-	}
-</style>
-
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
 <div class="h-dvh flex flex-col overscroll-contain overflow-auto">
 	{@render children()}
 </div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,14 +1,13 @@
 <script lang="ts">
-	import Loader from '$lib/loader.svelte';
-	import { shortenURL } from './shorten';
+  import { SERVER_URL } from '$lib/config';
 
-	let loading = false;
-	let long_url = '';
-	let short_url_created = false;
-	let short_url = '';
-	let error_message = '';
-	let errored = false;
-	let copied = false;
+	let loading = $state(false);
+	let long_url: string = $state('');
+	let short_url_created = $state(false);
+	let short_url = $state('');
+	let error_message = $state('');
+	let errored = $state(false);
+	let copied = $state(false);
 
 	async function handleURLSubmit(event: Event) {
 		error_message = '';
@@ -26,6 +25,20 @@
 		loading = false;
 	}
 
+async function shortenURL(url: string) {
+	const response = await fetch(`${SERVER_URL}/urls`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({ url })
+	});
+	if (!response.ok) {
+		throw new Error(response.status + response.statusText);
+	}
+	return response.json();
+}
+
 	function copyToClipboard(text: string) {
 		navigator.clipboard.writeText(text).then(() => {copied = true}).catch((err) => {
 			console.error('Failed to copy: ', err);
@@ -35,34 +48,35 @@
 	}
 </script>
 
-<div class="flex h-full w-full flex-col items-center justify-center gap-20">
+<div class="flex h-full w-full flex-col items-center justify-center gap-16">
 	<div class="flex flex-col items-center gap-4">
-		<h1 class="text-8xl">PithyURL</h1>
+		<h1 class="text-8xl text-primary">PithyURL</h1>
 		<p class="text-4xl">Create Short URLs!</p>
 	</div>
-
-	<div class="h-50 flex flex-row pb-2">
+	<div class="h-50 flex pb-2">
 		<div class="flex h-full flex-col items-center overflow-hidden py-2 transition-all duration-1000 ease-in-out {!short_url_created && !loading && !errored ? 'w-full' : 'w-0'}">
-			<form onsubmit={handleURLSubmit} class="flex h-full flex-col items-center gap-4 px-8">
-				<input id="longURL" type="url" class="w-96 rounded-full border-2 border-white bg-transparent px-5 py-1 text-3xl focus:outline-none" bind:value={long_url} required />
-				<button type="submit" class="rounded-full border-2 border-white bg-white px-8 text-3xl text-black transition hover:scale-110">→</button>
+			<form onsubmit={handleURLSubmit} class="flex h-full flex-col items-center gap-8 px-8">
+				<input id="longURL" type="url" class="w-96 input py-8 input-primary text-3xl" bind:value={long_url} required />
+				<button type="submit" class="px-8 text-3xl btn btn-primary btn-wide">→</button>
 			</form>
 		</div>
 
 		<div class="flex h-full flex-col items-center overflow-hidden py-2 transition-all duration-1000 ease-in-out {errored && !loading ? 'w-full' : 'w-0'}">
 			<div class="flex flex-col items-center gap-4">
-				<p class="text-nowrap text-3xl">{error_message}</p>
-				<button onclick={() => (errored = false)} class="rounded-full border-2 border-white px-8 text-3xl transition hover:scale-110">←</button>
+				<p class="text-nowrap text-error text-3xl">{error_message}</p>
+				<button onclick={() => (errored = false)} class="btn btn-primary btn-wide px-8 text-3xl ">←</button>
 			</div>
 		</div>
 
 		<div class="flex h-full flex-col items-center overflow-hidden py-2 transition-all duration-1000 ease-in-out {short_url_created && !loading ? 'w-full' : 'w-0'}">
 			<div class="flex h-full flex-col items-center gap-4 px-8">
-				<a href={short_url} target="_blank" class="text-nowrap rounded-full bg-white px-5 py-1 text-2xl text-black transition hover:scale-110">{short_url}</a>
-				<button onclick={() => copyToClipboard(short_url)} class="rounded-full border-2  px-16 text-3xl transition hover:scale-110 {copied ? 'text-green-600 border-green-600' : 'text-white border-white'}">⧉</button>
-				<button onclick={() => (short_url_created = false)} class="rounded-full border-2 border-white px-8 text-3xl transition hover:scale-110">←</button>
+				<a href={short_url} target="_blank" class="text-nowrap text-3xl link link-primary">{short_url}</a>
+				<button onclick={() => copyToClipboard(short_url)} class="btn btn-wide text-3xl transition  btn-outline {copied ? 'btn-success' : ''}">⧉</button>
+				<button onclick={() => (short_url_created = false)} class="btn btn-success btn-wide text-3xl ">←</button>
 			</div>
 		</div>
-		<!-- <div class="transition-all duration-1000 ease-in-out {loading ? 'w-full' : 'w-0'}"><Loader /></div> -->
+		<div class="flex h-full flex-col items-center overflow-hidden duration-1000 transiton ease-in-out {loading ? 'w-full ' : 'w-0'}">
+      <span class="loading loading-spinner loading-xl h-16 w-16"></span>
+    </div>
 	</div>
 </div>
